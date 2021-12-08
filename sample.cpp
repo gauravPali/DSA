2b9a350ab1252bf2e81aba93f58bff6a4103d3a5
Gaurav Pali <paligaurav97@gmail.com>
2021-12-08 21:42:34 +0530

    Sample cpp file added

--- sample.cpp
+++ sample.cpp
@@ -3,6 +3,8 @@ using namespace std;
 void solve() {
 }
 int main() {
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
     int t;
     cin >> t;
     while (t--) {
